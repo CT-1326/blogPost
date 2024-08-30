c9caeb26a0dcdeec42523a19ce43e65b10ffc578
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-08-30 11:10:20 +0900

    connect mongoDB

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  getStatus(): string {
+    const status = this.connection.readyState;
+    switch (status) {
+      case 0:
+        return 'disconnected';
+      case 1:
+        return 'connected';
+      case 2:
+        return 'connecting';
+      case 3:
+        return 'disconnecting';
+      default:
+        return 'unknown';
+    }
   }
 }
