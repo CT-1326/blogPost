a90c7067a10e9bf10d956f06b96cf1f1fdfd5c13
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-08-30 11:29:55 +0900

    env setUP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseConfigService } from './config/MongooseConfigService';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true, // 환경변수 캐싱 설정
+      isGlobal: true, // 환경변수 전역 설정
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
