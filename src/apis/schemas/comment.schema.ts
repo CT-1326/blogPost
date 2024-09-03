6ae7222b3ecef02167d1bce7037a5dd243f420ef
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-09-03 22:02:10 +0900

    [comment, category, tag] schema

---
+++ src/apis/schemas/comment.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { User } from './user.schema';
+import { Post } from './post.schema';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema()
+export class Comment {
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ type: Types.ObjectId, ref: Post.name, required: true })
+  post: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: User.name, required: true })
+  author: Types.ObjectId;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
