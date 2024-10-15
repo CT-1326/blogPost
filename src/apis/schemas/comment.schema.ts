a42a16302f341d1b3975bdc3549e5fdb2aaf8b59
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-10-15 16:17:54 +0900

    Update comment.schema.ts

--- src/apis/schemas/comment.schema.ts
+++ src/apis/schemas/comment.schema.ts
@@ -8,10 +8,10 @@ export class Comment {
   @Prop({ required: true })
   content!: string;
 
-  @Prop({ type: Types.ObjectId, ref: 'Post', required: true, index: true })
+  @Prop({ type: Types.ObjectId, ref: 'Post', required: true })
   post!: Types.ObjectId;
 
-  @Prop({ type: Types.ObjectId, ref: 'User', required: true, index: true })
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   author!: Types.ObjectId;
 
   @Prop({ default: false })
