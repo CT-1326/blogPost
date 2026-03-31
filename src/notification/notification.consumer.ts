2394a0267d5a0e59664c5189c9eb8496ead0ff9f
CT-1326 <smw7567@gmail.com>
2026-03-31 17:07:14 +0900

    Update notification.consumer.ts

--- src/notification/notification.consumer.ts
+++ src/notification/notification.consumer.ts
@@ -6,12 +6,12 @@ export class NotificationConsumer {
   @EventPattern('post.created')
   async handlePostCreated(@Payload() data: any) {
     // 전체 사용자에게 알림 전송
-    console.log('새로운 게시물이 작성됨!', data);
+    console.log('새로운 게시물이 업로드 되었습니다.', data);
   }
 
   @EventPattern('comment.created')
   async handleCommentCreated(@Payload() data: any) {
     // 해당 게시물 작성자에게 알림 전송
-    console.log('게시물에 댓글이 작성됨!', data);
+    console.log('사용자 게시물에 댓글이 작성되었습니다.', data);
   }
 }
